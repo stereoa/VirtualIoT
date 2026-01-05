693f6b0b57375ec30eaf15fc3ad3a38abedb786b
Geoffrey Hibbert <stereotypeaura@gmail.com>
2026-01-05 13:09:34 -0500

    Basic usage of MQTT service and virtual device

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Threading.Tasks;
+using VirtualIoT;
+using VirtualIoT.Models.Virtual;
+
+class Program
+{
+    static async Task Main()
+    {
+        var mqtt = new MqttService();
+
+        var device = new VirtualDevice("device1", mqtt);
+        device.AddSensor(new VirtualTemperatureSensor());
+        device.AddSensor(new VirtualHumiditySensor());
+
+        await device.InitializeAsync();
+        await device.RunAsync();
+    }
+}
